fe178913c773931d92fedb3d0b2a7314a1e19ff8
SDC@StPaulsSchool.org.uk <SDC@StPaulsSchool.org.uk>
2020-10-08 15:19:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO; // Add for file I/O operations
+
+namespace FilesExamplesSDC
+{
+    class Program
+    {
+        static void WriteSentences()
+        {
+            string nextLine;
+            string myFile = "sentences.txt";
+
+            // Version 1
+            // StreamWriter fileStr = File.CreateText(myFile);
+
+            // Version 2 write to My Documents
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            myFile = Path.Combine(docPath, myFile);
+            StreamWriter fileStr = File.CreateText(myFile);
+
+            do
+            {
+                Console.WriteLine("Enter next sentence: ");
+                nextLine = Console.ReadLine();
+                if (nextLine != "")
+                {
+                    fileStr.WriteLine(nextLine);
+                }
+            } while (nextLine != "");
+            fileStr.Close();
+        }
+        static void Main()
+        {
+            Console.WriteLine("File manipulation");
+            WriteSentences();
+        }
+    }
+}
